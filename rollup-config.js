6ffebeccfdc866c3fee1d7fe057fc89243741ff0
Nicholas Jamieson <nicholas@cartant.com>
2017-05-08 18:11:24 +1000

    chore(build): Remove Rollup ignore.

--- rollup-config.js
+++ rollup-config.js
@@ -1,4 +1,3 @@
-import ignore from "rollup-plugin-ignore";
 import nodeResolve from "rollup-plugin-node-resolve";
 import { external, globals } from "./rollup-constants";
 
@@ -10,8 +9,5 @@ export default {
     format: "umd",
     globals: Object.assign({}, globals),
     moduleName: "firebaseThermiteNg",
-    plugins: [
-        ignore(["firebase"]),
-        nodeResolve({})
-    ]
+    plugins: [nodeResolve({})]
 }
